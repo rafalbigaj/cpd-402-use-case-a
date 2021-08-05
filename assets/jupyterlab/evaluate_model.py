a78a1bae9b367b23ce0da4a86ba951ba26e45ea5
admin <-->
2021-08-05 14:47:10 +0000

    feat: evaluation results in batch deployment output

--- assets/jupyterlab/evaluate_model.py
+++ assets/jupyterlab/evaluate_model.py
@@ -1,1 +1,12 @@
-print('Model validation succeeded!')
+import os
+
+output_dir = os.getenv('BATCH_OUTPUT_DIR', './')
+
+results = 'Model validation succeeded!'
+
+results_txt = os.path.join(output_dir, 'results.txt')
+
+with open(results_txt, 'w') as f:
+    print(results, file=f)
+
+print(results)
